2cfa01bac0a709b95bba0532b28d27e3e015f723
Josip Gracin <josip.gracin@gmail.com>
2011-08-31 10:43:55 +0200

    compojure implementacija koja radi

--- src/main/clojure/com/ingemark/accm.clj
+++ src/main/clojure/com/ingemark/accm.clj
@@ -1,13 +1,24 @@
 (ns com.ingemark.accm
-  (:use (compojure core [route :as route])
+  (:use com.ingemark.clojure.logger
+        (compojure core [route :as route])
         [com.ingemark.ring-jetty-adapter :only (run-jetty)]
         (ring.util [servlet :as servlet])
         [ring.middleware file file-info [params :only (wrap-params)]]
         [clojure.contrib.json :only (json-str read-json)])
   (:import (org.mortbay.jetty Server Request Response)
-           (org.mortbay.jetty.handler AbstractHandler)))
+           (org.mortbay.jetty.handler AbstractHandler)
+           (org.asteriskjava.manager ManagerConnectionFactory ManagerConnection
+                                     ManagerEventListener)
+           (org.asteriskjava.manager.event ManagerEvent)))
+
+(defrecord AppState [^ManagerConnectionFactory manager-connection-factory
+                     ^ManagerConnection manager-connection])
+
+(defonce appstate (atom (AppState. nil nil)))
 
 (defn m [cnt] (str "{\"id\":\"" cnt "\", \"type\":\"update\"}"))
+
+(def queues (atom []))
 
 (defn- printout [w]
   (doseq [cnt (range 30)]
@@ -17,23 +28,43 @@
 
 (defroutes main-routes
   (GET "/events" request
-       (do
+       (let [myq (java.util.concurrent.LinkedBlockingQueue.)]
+         (swap! queues conj myq)
          (when-let [response (:response request)]
            (when-let [w (.getOutputStream response)]
-             (printout w)))
+             (loop []
+               (let [msg (.take myq)]
+                 (.write w (.getBytes msg))
+                 (.flush w))
+               (recur))))
          {:status 200 :body (java.io.ByteArrayInputStream. (.getBytes ""))}))
   (POST "/call" request
         {:status 200 :body "Call originated."}))
 
 (defonce server (atom nil))
 
+(defn- handle-ami-event
+  "Called on each AMI event that we receive from asterisk-java framework."
+  [^ManagerEvent event]
+  (logdebug "received AMI event")
+  (doseq [q @queues]
+    (.offer q (str (class event)))))
+
 (defn main []
   (if @server
     "Server already running"
-    (reset! server
-            (run-jetty
-             (-> (routes (var main-routes) (route/not-found "Page not found")))
-             {:port 8082 :join? false}))))
+    (do
+      (let [factory (ManagerConnectionFactory. "192.168.18.30" "manager" "oracle")
+            connection (.createManagerConnection factory)]
+        (swap! appstate
+               assoc :manager-connection-factory factory :manager-connection connection)
+        (.login connection)
+        (.addEventListener connection (proxy [ManagerEventListener] []
+                                        (onManagerEvent [event] (handle-ami-event event)))))
+      (reset! server
+              (run-jetty
+               (-> (routes (var main-routes) (route/not-found "Page not found")))
+               {:port 8082 :join? false})))))
 
 (defn stop []
   (if-not @server
@@ -41,9 +72,3 @@
     (do (doto @server .stop .join)
         (reset! server nil))))
 
-(defn connect []
-  (let [client (org.apache.http.impl.client.DefaultHttpClient.)
-        method (org.apache.http.client.methods.HttpGet. "http://localhost:8082/events")
-        response (.execute client method)
-        entity (.getEntity response)]
-    entity))
