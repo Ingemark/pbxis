bc676f02e9d94bfa4ea36ba01b2883b097036baa
Marko Topolnik <marko.topolnik@inge-mark.hr>
2012-09-01 11:04:54 +0200

    New solution

--- src/main/clojure/com/ingemark/pbxis/service.clj
+++ src/main/clojure/com/ingemark/pbxis/service.clj
@@ -1,62 +1,56 @@
 (ns com.ingemark.pbxis.service
+  (require [clojure.set :as set]
+           [com.ingemark.clojure.logger :refer :all])
   (import [org.asteriskjava.manager.event
-           ManagerEvent JoinEvent LeaveEvent BridgeEvent HangupEvent QueueMemberStatusEvent
+           ManagerEvent JoinEvent LeaveEvent DialEvent HangupEvent
            AgentCalledEvent AgentConnectEvent AgentCompleteEvent]))
 
 (def empty-q clojure.lang.PersistentQueue/EMPTY)
 
-(defonce agnt-event-qs (atom {}))
+(def q-agnts (atom {"3000" #{"701"}
+                    "3001" #{"701" "702"}}))
 
-(defonce agnt-props (atom {}))
+(defonce agnt-eventq (atom {}))
 
-(defonce caller-q (atom empty-q))
+(defn- update-qagnts [q-agnts agnt qs]
+  (let [conj #(conj (or %1 #{}) %2)
+        qs-for-add (into #{} qs)
+        qs-for-remove (set/difference (into #{} (keys q-agnts)) qs-for-add)
+        q-agnts (reduce #(update-in %1 [%2] conj agnt) q-agnts qs-for-add)]
+    (reduce #(if (= (%1 %2) #{agnt}) (dissoc %1 %2) (update-in %1 [%2] disj agnt))
+            q-agnts qs-for-remove)))
 
-(defonce call-props (atom {}))
-
-(defn- update-agnt-state [agnt status unique-id & channel]
-  (let [newstate {:status status :unique-id unique-id}
-        newstate (if (empty? channel) newstate (assoc newstate :channel (first channel)))]
-    (if-let [state (agnt-props agnt)]
-      (apply swap! state assoc (seq newstate))
-      (swap! agnt-props assoc agnt (atom newstate)))))
+(defn config-agnt [agnt & qs]
+  (swap! agnt-eventq (fn [agnt-eventq] (update-in agnt-eventq [agnt] #(or % empty-q))))
+  (swap! q-agnts update-qagnts agnt qs))
 
 (defn- enq-event [agnt k & vs]
-  (let [ev (vec (cons k vs))]
-    (if-let [q (agnt-event-qs agnt)]
-      (swap! q conj ev)
-      (swap! agnt-event-qs assoc agnt (atom (conj empty-q ev))))))
+  (when-let [q (agnt-eventq (re-find #"\d+$" agnt))] (swap! q conj (vec (cons k vs)))))
+
+(defn- broadcast-qcount [q ami-event]
+  (doseq [agnt (@q-agnts (ami-event :queue))] (enq-event agnt "queueCount" (ami-event :count))))
 
 (defn handle-ami-event [^ManagerEvent event-object]
   (let [event (bean event-object)
-        id (event :uniqueId)
-        cnt (event :count)
-        agnt (or (event :agentCalled) (event :member))]
+        unique-id (event :uniqueId)]
     (condp instance? event-object
-      LeaveEvent
-      (enq-event agnt "queueChanged" cnt)
-      AgentCalledEvent
-      (do (update-agnt-state "RINGING" id (event :destinationChannel))
-          (enq-event agnt "ringing" (:caller-id (get @call-props id))))
       JoinEvent
-      (do (swap! call-props assoc id {:caller-id (event :callerIdNum), :queue (event :queue)})
-          (enq-event agnt "queueChanged" cnt))
-      AgentConnectEvent
-      (do (update-agnt-state "BUSY" id)
-          (enq-event agnt "callAccepted" (:caller-id (get @call-props id))))
-      AgentCompleteEvent
-      (do (update-agnt-state "FREE" nil nil)
-          (enq-event agnt "agentCallOver"
-                     (:caller-id (@call-props id))
-                     (event :talkTime)
-                     (event :holdTime)
-                     (.get (event :variables) "FILEPATH"))
-          (enq-event agnt "agentAvailable")
-          (swap! call-props dissoc id))
+      (broadcast-qcount event)
+      LeaveEvent
+      (broadcast-qcount event)
+      DialEvent
+      (when (= (event :subEvent) "Begin")
+        (enq-event (event :callerIdNum) "outgoingCall"))
       HangupEvent
-      (let [agnt-props (@agnt-props agnt)]
-        (when (or (= id (:unique-id agnt-props))
-                  (= (event :channel) (agnt-props :channel)))
-          (update-agnt-state agnt "FREE" nil nil)
-          (swap! call-props dissoc id)
-          (enq-event agnt "agentAvailable")))
-      (println (str "Event will not be processed" (class event))))))
+      (enq-event (event :callerIdNum) "outgoingCallEnded")
+      AgentCalledEvent
+      (enq-event (event :agentCalled) "incomingCall" unique-id (event :callerIdNum))
+      AgentConnectEvent
+      (enq-event (event :member) "incomingCallAccepted" unique-id)
+      AgentCompleteEvent
+      (enq-event (event :member) "incomingCallEnded"
+                 unique-id
+                 (event :talkTime)
+                 (event :holdTime)
+                 (.get (event :variables) "FILEPATH"))
+      (logdebug "Ignoring event" (.getSimpleName (type event-object))))))
