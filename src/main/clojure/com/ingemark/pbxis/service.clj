9ae2e6859b98f42702886ef39ea4235a2489cd4b
Marko Topolnik <marko.topolnik@inge-mark.hr>
2012-09-04 15:31:24 +0200

    New event processing

--- src/main/clojure/com/ingemark/pbxis/service.clj
+++ src/main/clojure/com/ingemark/pbxis/service.clj
@@ -1,19 +1,27 @@
 (ns com.ingemark.pbxis.service
   (require [clojure.set :as set]
-           [clojure.core.incubator :refer (-?>)]
+           (clojure.core [incubator :refer (-?>)] [strint :refer (<<)])
            [com.ingemark.clojure.logger :refer :all])
   (import org.asteriskjava.manager.event.ManagerEvent
           (java.util.concurrent ConcurrentHashMap LinkedBlockingQueue TimeUnit)))
 
+(def POLL-TIMEOUT-SEC 15)
+(def UNSUB-DELAY-SEC (+ POLL-TIMEOUT-SEC 2))
+(def EVENT-BURST-MILLIS 50)
+
 (defn- empty-q [] (LinkedBlockingQueue.))
 
-(def amiq-agnts (atom {}))
+(defonce lock (Object.))
 
-(def agnt-eventq (atom {}))
+(defonce scheduler (atom nil))
 
-(defonce sched (atom nil))
+(defonce amiq-agnts (atom {}))
 
-(defonce agnt-unsubscriber (ConcurrentHashMap.))
+(defonce agnt-state (atom {}))
+
+(defonce rndkey-agnt (atom {}))
+
+(defonce agnt-unsubscriber (atom {}))
 
 (defn- update-amiq-agnts [amiq-agnts agnt amiqs]
   (let [conj #(conj (or %1 #{}) %2)
@@ -23,75 +31,98 @@
     (reduce #(if (= (%1 %2) #{agnt}) (dissoc %1 %2) (update-in %1 [%2] disj agnt))
             amiq-agnts amiqs-for-remove)))
 
-(defn- enq-event [agnt k & vs]
-  (when-let [q (@agnt-eventq (re-find #"\d+$" (or agnt "")))]
-    (.add q (spy "enq-event" agnt (vec (cons k vs))))))
+(declare config-agnt)
+
+(defn- reschedule-agnt-unsubscriber [agnt]
+  (let [newsched (.schedule @scheduler (fn [] (config-agnt agnt []))
+                            UNSUB-DELAY-SEC TimeUnit/SECONDS)]
+    (swap! agnt-unsubscriber update-in [agnt] #(do (-?> % (.cancel false)) newsched))))
+
+(defn config-agnt [agnt qs]
+  (logdebug "config-agnt" agnt qs)
+  (locking lock
+    (swap! amiq-agnts update-amiq-agnts agnt qs)
+    (let [s (@agnt-state agnt)]
+      (if (seq qs)
+        (do (when-not s
+              (let [rndkey (-> (java.util.UUID/randomUUID) .toString)]
+                (swap! rndkey-agnt assoc rndkey agnt)
+                (swap! agnt-state assoc agnt {:rndkey rndkey :eventq (empty-q)})))
+            (reschedule-agnt-unsubscriber agnt)
+            {:agent agnt :key ((@agnt-state agnt) :rndkey) :queues qs})
+        (do
+         (swap! rndkey-agnt dissoc (:rndkey s))
+         (swap! agnt-state dissoc agnt)
+         (<< "Agent ~{agnt} unsubscribed"))))))
+
+(defn events-for [key]
+  (locking lock
+    (when-let [agnt (@rndkey-agnt key)]
+      (let [q (-> (@agnt-state agnt) :eventq)]
+        (reschedule-agnt-unsubscriber agnt)
+        (let [drain-events #(.drainTo q %)
+              evs (doto (java.util.ArrayList.) drain-events)]
+          (when-not (seq evs)
+            (when-let [head (.poll q POLL-TIMEOUT-SEC TimeUnit/SECONDS)]
+              (Thread/sleep EVENT-BURST-MILLIS)
+              (doto evs (.add head) drain-events)))
+          evs)))))
+
+(def ignored-events
+  #{"VarSet" "NewState" "NewChannel" "NewExten" "NewCallerId" "NewAccountCode" "ChannelUpdate"
+    "RtcpSent" "RtcpReceived" "QueueMemberStatus"})
 
 (defn- broadcast-qcount [ami-event]
   (let [amiq (ami-event :queue)]
     (doseq [agnt (@amiq-agnts amiq)] (enq-event agnt "queueCount" amiq (ami-event :count)))))
 
-(declare config-agnt)
+(defn- digits [s] (re-find #"\d+" (or s "")))
 
-(defn- reschedule-agnt-unsubscriber [agnt]
-  (-?> (.put agnt-unsubscriber agnt (.schedule @sched #(config-agnt agnt []) 5 TimeUnit/SECONDS))
-       (.cancel false)))
-
-(defn config-agnt [agnt qs]
-  (logdebug "config-agnt" agnt qs)
-  (swap! agnt-eventq (if (seq qs)
-                       #(if (% agnt) % (assoc % agnt (empty-q)))
-                       #(dissoc % agnt)))
-  (swap! amiq-agnts update-amiq-agnts agnt qs)
-  (when (seq qs) (reschedule-agnt-unsubscriber agnt))
-  {:agent agnt :queues qs})
-
-(defn events-for [agnt]
-  (when-let [q (@agnt-eventq agnt)]
-    (reschedule-agnt-unsubscriber agnt)
-    (let [drain-events #(.drainTo q %)
-          evs (doto (java.util.ArrayList.) drain-events)]
-      (if (seq evs)
-        evs
-        (when-let [head (.poll q 4 TimeUnit/SECONDS)]
-          (Thread/sleep 50)
-          (doto evs (.add head) drain-events))))))
+(defn- enq-event [agnt k & vs]
+  (when-let [q (-?> (@agnt-state (digits agnt)) :eventq)]
+    (.add q (spy "enq-event" agnt (vec (cons k vs))))))
 
 (defn handle-ami-event [event]
-  (let [unique-id (event :uniqueId)]
-    (condp = (:event-type event)
-      "Join"
-      (broadcast-qcount event)
-      "Leave"
-      (broadcast-qcount event)
-      "Dial"
-      (when (= (event :subEvent) "Begin")
-        (enq-event (event :callerIdNum) "outgoingCall"))
-      "Hangup"
-      (enq-event (event :callerIdNum) "outgoingCallEnded")
-      "AgentCalled"
-      (enq-event (event :agentCalled) "incomingCall" unique-id (event :callerIdNum))
-      "AgentConnect"
-      (enq-event (event :member) "incomingCallAccepted" unique-id)
-      "AgentComplete"
-      (enq-event (event :member) "incomingCallEnded"
-                 unique-id
-                 (event :talkTime)
-                 (event :holdTime)
-                 (-?> event :variables (.get "FILEPATH")))
-      nil #_(logdebug "Ignoring event" (:event-type event)))))
+  (let [t (:event-type event)]
+    (when-not (ignored-events t)
+      (logdebug
+       "AMI event\n" t
+       (into (sorted-map)
+             (dissoc event :event-type :dateReceived :privilege :application :server :context
+                     :priority :appData :func :class :source :timestamp :line :file :sequenceNumber))))
+    (let [unique-id (event :uniqueId)]
+      (condp = t
+        "Join"
+        (broadcast-qcount event)
+        "Leave"
+        (broadcast-qcount event)
+        "Dial"
+        (when (= (event :subEvent) "Begin")
+          (enq-event (event :channel) "dialOut" (event :srcUniqueId) (-> event :dialString digits))
+          (enq-event (event :destination) "calledDirectly" (event :destUniqueId) (event :callerIdNum)))
+        "Hangup"
+        (enq-event (event :channel) "hangup" unique-id)
+        "AgentCalled"
+        (enq-event (event :agentCalled) "agentCalled" unique-id (event :callerIdNum))
+        "AgentConnect"
+        (enq-event (event :member) "agentConnect" unique-id)
+        "AgentComplete"
+        (enq-event (event :member) "agentComplete"
+                   unique-id (event :talkTime) (event :holdTime)
+                   (-?> event :variables (.get "FILEPATH")))
+        nil))))
 
 (defn etest []
-  #_((config-agnt "148" ["700" "3001"])
+  #_((config-agnt "701" "148" ["700" "3001"])
   (config-agnt "201" ["3000"]))
   (handle-ami-event {:event-type "Join" :queue "700" :count 1})
   (Thread/sleep 4)
-  (handle-ami-event {:event-type "AgentCalled" :agentCalled "SCCP/148"
+  (handle-ami-event {:event-type "AgentCalled" :agentCalled "SCCP/701"
                      :uniqueId "a" :callerIdNum "111111"})
   (Thread/sleep 4)
   (handle-ami-event {:event-type "Leave" :queue "700" :count 0})
   (Thread/sleep 4)
-  (handle-ami-event {:evet-type "AgentConnect" :member "SCCP/148" :uniqueId "a"})
+  (handle-ami-event {:evet-type "AgentConnect" :member "SCCP/701" :uniqueId "a"})
   (Thread/sleep 4)
-  (handle-ami-event {:event-type "AgentComplete" :member "SCCP/148" :uniqueId "a"
+  (handle-ami-event {:event-type "AgentComplete" :member "SCCP/701" :uniqueId "a"
                      :talkTime 20 :holdTime 2}))
