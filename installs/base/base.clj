b4c85cd4bd395284afa13acafde2abd3d966ee37
Marko Topolnik <marko.topolnik@inge-mark.hr>
2012-09-11 21:12:13 +0200

    Changed polling interval

--- installs/base/base.clj
+++ installs/base/base.clj
@@ -2,7 +2,7 @@
       :channel-prefix "SCCP/"
       :originate-context "internal"
       :originate-timeout-seconds 45
-      :poll-timeout-seconds 15
+      :poll-timeout-seconds 30
       :ami {:ip-address "192.168.18.30"
             :username   "accm"
             :password   "h2e9d49"}}
