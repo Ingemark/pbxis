a4fe4bc333dcb21450c3de6a74a9d9354049ceea
Marko Topolnik <mtopolnik@inge-mark.hr>
2012-09-25 13:23:33 +0200

    Change test config

--- base.clj
+++ base.clj
@@ -2,8 +2,8 @@
       :channel-prefix "SCCP/"
       :originate-context "internal"
       :originate-timeout-seconds 45
-      :poll-timeout-seconds 5
-      :agent-gc-delay-minutes 15
+      :poll-timeout-seconds 2
+      :agent-gc-delay-minutes 1
       :ami {:ip-address "192.168.18.30"
             :username   "accm"
             :password   "h2e9d49"}}
