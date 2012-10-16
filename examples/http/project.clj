e944cc8fef4c19990499e91b06f80c9817460b4e
Marko Topolnik <mtopolnik@inge-mark.hr>
2012-10-16 12:49:42 +0200

    Added http long-polling example

--- examples/http/project.clj
+++ examples/http/project.clj
@@ -1,12 +1,15 @@
 (defproject com.ingemark/pbxis "0.1.0-SNAPSHOT"
-  :description "Asterisk Call Center Adapter"
+  :description "Call Center Demo over HTTP with long polling"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.2"]
+                 [org.clojure/data.json "0.1.3"]
+                 [net.cgrand/moustache "1.1.0"]
+                 [ring/ring-jetty-adapter "1.1.0" :exclusions [javax.servlet/servlet-api]]
+                 [ring-json-params/ring-json-params "0.1.3"]
                  [org.slf4j/slf4j-api "1.6.1"]
-                 [org.slf4j/log4j-over-slf4j "1.6.4"]
                  [ch.qos.logback/logback-classic "1.0.3"]
-                 [org.asteriskjava/asterisk-java "1.0.0.M3" :exclusions [[log4j/log4j]]]]
+                 [com.ingemark/pbxis "0.1.0-SNAPSHOT"]]
   :plugins [[lein-swank "1.4.4"]])
---
+++ examples/http/src/com/ingemark/pbxis_http.clj
@@ -0,0 +1,55 @@
+(ns com.ingemark.pbxis-http
+  (require (com.ingemark [pbxis :as px] [logging :refer :all])
+           [clojure.data.json :as json]
+           [ring.util.response :as r]
+           [net.cgrand.moustache :refer (app)]
+           [ring.adapter.jetty :refer (run-jetty)]
+           (ring.middleware [json-params :refer (wrap-json-params)]
+                            [file :refer (wrap-file)]
+                            [file-info :refer (wrap-file-info)])))
+
+(defn ok [f & args] (fn [_] (let [ret (apply f args)]
+                              ((if (nil? ret) r/not-found r/response) (json/json-str ret)))))
+
+(defn wrap-log-request [handle] #(->> % (spy "HTTP request") handle (spy "HTTP response")))
+
+(def app-main
+  (app
+   wrap-json-params
+   ["agent" id &]
+   [[] {:post [{:strs [queues]} (ok px/config-agnt id queues)]
+        :get (ok px/events-for id)}
+    ["originate"] {:post [{:strs [phone]} (ok px/originate-call id phone)]}
+    ["queue" action] {:post [{:as params}
+                             (ok px/queue-action action id
+                                 (select-keys params
+                                              (into ["queue"]
+                                                    (condp = action
+                                                      "add" ["memberName" "paused"]
+                                                      "pause" ["paused"]
+                                                      "remove" []))))]}]))
+
+(defonce server (atom nil))
+
+(defn stop []
+  (println "Shutting down")
+  (when @server (doto @server .stop .join) (reset! server nil))
+  (px/ami-disconnect)
+  nil)
+
+(defn main []
+  (stop)
+  (let [host "192.168.100.101"
+        username "admin"
+        password "protect"
+        cfg {:port 58615
+             :channel-prefix "SCCP/"
+             :originate-context "default"
+             :originate-timeout-seconds 45
+             :poll-timeout-seconds 2
+             :agent-gc-delay-minutes 1}]
+    (px/ami-connect host username password cfg))
+  (reset! server (run-jetty (-> (var app-main)
+                                (wrap-file "static-content")
+                                wrap-file-info)
+                            {:join? false})))
