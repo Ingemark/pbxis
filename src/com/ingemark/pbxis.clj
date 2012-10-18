a3562533386263872ef5271cb51a1556fb162364
Marko Topolnik <mtopolnik@inge-mark.hr>
2012-10-18 17:17:32 +0200

    Improving asynchrounous event notification

--- src/com/ingemark/pbxis.clj
+++ src/com/ingemark/pbxis.clj
@@ -15,7 +15,7 @@
            (clojure.core [incubator :refer (-?> -?>> dissoc-in)] [strint :refer (<<)]))
   (import (org.asteriskjava.manager ManagerConnectionFactory ManagerEventListener)
           org.asteriskjava.manager.event.ManagerEvent
-          (java.util.concurrent LinkedBlockingQueue TimeUnit)
+          (java.util.concurrent Executors LinkedBlockingQueue TimeUnit)
           (clojure.lang Reflector RT)))
 
 (defonce config (atom {:channel-prefix "SCCP/"
@@ -35,6 +35,8 @@
 
 (defonce ^:private lock (Object.))
 
+(defonce thread-pool (atom nil))
+
 (defonce scheduler (atom nil))
 
 (defonce ami-connection (atom nil))
@@ -43,7 +45,7 @@
 
 (defonce ^:private agnt-state (atom {}))
 
-(defonce ^:private rndkey-agnt (atom {}))
+(defonce ^:private ticket-agnt (atom {}))
 
 (defonce ^:private agnt-unsubscriber (atom {}))
 
@@ -143,10 +145,10 @@
                 #(locking lock
                    (loginfo "Unsubscribe agent" agnt)
                    (when-let [now-state (@agnt-state agnt)]
-                     (swap! rndkey-agnt dissoc (now-state :rndkey))
-                     (update-agnt-state agnt dissoc :rndkey :eventq)))))
+                     (swap! ticket-agnt dissoc (now-state :ticket))
+                     (update-agnt-state agnt dissoc :ticket :eventq :listener)))))
 
-(defn- rndkey [] (-> (java.util.UUID/randomUUID) .toString))
+(defn- ticket [] (-> (java.util.UUID/randomUUID) .toString))
 
 (def int->exten-status {0 "not_inuse" 1 "inuse" 2 "busy" 4 "unavailable" 8 "ringing" 16 "onhold"})
 
@@ -227,17 +229,39 @@
       (when change? (update-agnt-state agnt assoc-in [:amiq-status q] st))
       change?)))
 
-(defn- replace-rndkey [agnt old new]
-  (swap! rndkey-agnt #(-> % (dissoc old) (assoc new agnt)))
-  (update-agnt-state agnt assoc :rndkey new))
+(defn- replace-ticket [agnt old new]
+  (swap! ticket-agnt #(-> % (dissoc old) (assoc new agnt)))
+  (update-agnt-state agnt assoc :ticket new))
+
+(defn- drain-events-to-listener [agnt]
+  (let [t (Object.)
+        swapped-in (update-agnt-state agnt update-in [:draining] (when-not % t))]
+    (when (= swapped-in t)
+      (.execute
+       @thread-pool
+       (fn []
+         (let [{:keys [eventq listener]} (@agnt-state agnt)]
+           (if [(and eventq listener)]
+             (let [evs (java.util.ArrayList.)]
+               (locking eventq
+                 (.drainTo eventq evs)
+                 (when-not (seq evs)
+                   (update-agnt-state agnt dissoc :draining)))
+               (try
+                 (doseq [ev evs] (listener ev))
+                 (catch Throwable t
+                   (logerror "Failure in event listener" t)
+                   (update-agnt-state agnt dissoc :listener)
+                   (reschedule-agnt-gc agnt)
+                   (set-agnt-unsubscriber-schedule agnt true))
+                 (finally (when (seq evs) recur))))
+             (update-agnt-state agnt dissoc :draining))))))))
 
 (defn- push-event [agnt k & vs]
-  (let [state (@agnt-state (digits agnt))
-        ev (vec (cons k vs))]
-    (when-let [q (-?> state :eventq)]
-      (.add q (spy "enqueue event" agnt ev)))
-    (when-let [ls (-?> state :listeners seq)]
-      (doseq [l ls] (spy "notify of event" agnt (l ev))))))
+  (let [agnt (digits agnt)]
+    (when-let [q (-?> agnt @agnt-state :eventq)]
+      (locking q (.add q (spy "enqueue event" agnt (vec (cons k vs)))))
+      (drain-events-to-listener agnt))))
 
 (defn config-agnt
   "Subscribes an agent to the event stream. If the agent is already
@@ -246,33 +270,25 @@ subscribed, reconfigures the subscription.
 agnt: agent's phone extension number.
 qs: a collection of agent queue names. If empty or nil, the agent is
     unsubscribed.
-listeners: optional callback functions that will be called with the
-           events as they arrive.
 
-Returns the key (string) to be passed to events-for.
-If any listeners are supplied, returns nil.
-If unsubscribing, returns a string message \"Agent {agent} unsubscribed\".
-
-When subscribing an agent, this function immediately generates all
-events necessary to establish the initial state. This means that the
-supplied listener functions will be called even before the call to
-this function returns."
-  [agnt qs & listeners]
+Returns the ticket (string) to be passed to events-for or register-listener.
+If unsubscribing, returns a string message \"Agent {agent} unsubscribed\"."
+  [agnt qs]
   (loginfo "config-agnt" agnt qs)
   (locking lock
     (swap! amiq-cnt-agnts update-amiq-cnt-agnts agnt qs)
-    (let [now-state (@agnt-state agnt), listeners (seq listeners)]
+    (let [now-state (@agnt-state agnt)]
       (if (seq qs)
-        (let [rndkey (if listeners nil (rndkey)), eventq (if listeners nil (empty-q))]
-          (set-agnt-unsubscriber-schedule agnt (not (nil? eventq)))
-          (if eventq (reschedule-agnt-gc agnt) (set-schedule agnt-gc agnt nil nil))
+        (let [ticket (ticket), eventq (empty-q)]
+          (set-agnt-unsubscriber-schedule agnt true)
+          (reschedule-agnt-gc agnt)
           (if now-state
-            (do (replace-rndkey agnt (:rndkey now-state) rndkey)
-                (update-agnt-state agnt assoc :eventq eventq :listeners listeners)
+            (do (replace-ticket agnt (:ticket now-state) ticket)
+                (update-agnt-state agnt assoc :eventq eventq)
                 (-?> now-state :eventq (.add ["requestInvalidated"])))
-            (do (when rndkey (swap! rndkey-agnt assoc rndkey agnt))
+            (do (when ticket (swap! ticket-agnt assoc ticket agnt))
                 (swap! agnt-state assoc agnt
-                       {:rndkey rndkey :eventq eventq :listeners listeners
+                       {:ticket ticket :eventq eventq
                         :exten-status (exten-status agnt)
                         :calls (calls-in-progress agnt)})))
           (when (not= (set qs) (-?> now-state :amiq-status keys set))
@@ -283,36 +299,36 @@ this function returns."
             (push-event agnt "phoneNumber" (-?> st :calls first val)))
           (push-event agnt "queueCount"
                       (into {} (for [[amiq {:keys [cnt]}] (select-keys @amiq-cnt-agnts qs)] [amiq cnt])))
-          rndkey)
+          ticket)
         (do
-          (swap! rndkey-agnt dissoc (:rndkey now-state))
+          (swap! ticket-agnt dissoc (:ticket now-state))
           (swap! agnt-state dissoc agnt)
           (<< "Agent ~{agnt} unsubscribed"))))))
 
 (defn events-for
-  "Synchronously receives events associated with a key. The function
+  "Synchronously receives events associated with a ticket. The function
 blocks until an event appears or the configured poll-timeout
-elapses. The supplied key is immediately invalidated and the next call
-must use the key returned by the current call.
+elapses. The supplied ticket is immediately invalidated and the next call
+must use the ticket returned by the current call.
 
-Returns a map {:key \"new-key\" :events [events]}.
+Returns a map {:ticket \"new-ticket\" :events [events]}.
 
 If the events vector contains a \"requestInvalidated\" event, that means
 that config-agnt was called concurrently for the agent associated
-with the current call to this function. In that case the key returned
+with the current call to this function. In that case the ticket returned
 by this call is already invalid.
 
-The key is also automatically invalidated if this function is not called
+The ticket is also automatically invalidated if this function is not called
 again after it returns. The timeout for this is half the poll-timeout."
-  [agnt-key]
-  (when-let [[agnt q newkey] (locking lock
-                               (when-let [agnt (@rndkey-agnt agnt-key)]
+  [ticket]
+  (when-let [[agnt q newtkt] (locking lock
+                               (when-let [agnt (@ticket-agnt ticket)]
                                  (when-let [q (>?> @agnt-state agnt :eventq)]
                                    (reschedule-agnt-gc agnt)
-                                   (let [rndkey (rndkey)]
-                                     (replace-rndkey agnt agnt-key rndkey)
+                                   (let [newtkt (ticket)]
+                                     (replace-ticket agnt ticket newtkt)
                                      (set-agnt-unsubscriber-schedule agnt false)
-                                     [agnt q rndkey]))))]
+                                     [agnt q newtkt]))))]
     (try
       (let [drain-events #(.drainTo q %)
             evs (doto (java.util.ArrayList.) drain-events)]
@@ -320,9 +336,24 @@ again after it returns. The timeout for this is half the poll-timeout."
           (when-let [head (.poll q (poll-timeout) TimeUnit/SECONDS)]
             (Thread/sleep EVENT-BURST-MILLIS)
             (doto evs (.add head) drain-events)))
-        (spy "Events for" agnt {:key newkey :events (vec evs)}))
-      (finally (when (= newkey (>?> @agnt-state agnt :rndkey))
+        (spy "Events for" agnt {:ticket newtkt :events (vec evs)}))
+      (finally (when (= newtkt (>?> @agnt-state agnt :ticket))
                  (set-agnt-unsubscriber-schedule agnt true))))))
+
+(defn register-listener
+  "Registers a listener to an agent's events. Requires a ticket.
+Returns a new ticket (the supplied one is invalidated)."
+  [ticket listener]
+  (locking lock
+    (when-let [agnt (@ticket-agnt ticket)]
+      (when (>?> @agnt-state agnt :eventq)
+        (update-agnt-state agnt assoc :listener listener)
+        (set-schedule agnt-gc agnt nil nil)
+        (let [newtkt (ticket)]
+          (replace-ticket agnt ticket newtkt)
+          (set-agnt-unsubscriber-schedule agnt false)
+          (drain-events-to-listener agnt)
+          newtkt)))))
 
 (defn- broadcast-qcount [ami-event]
   (locking lock
@@ -412,7 +443,7 @@ again after it returns. The timeout for this is half the poll-timeout."
                          (push-event agnt "queueMemberStatus" {q st})))))
         nil))))
 
-(defn- actionid [] (<< "pbxis-~(.substring (rndkey) 0 8)"))
+(defn- actionid [] (<< "pbxis-~(.substring (ticket) 0 8)"))
 
 (defn originate-call
   "Issues a request to originate a call to the supplied phone number and
@@ -491,7 +522,8 @@ cfg: a map of configuration parameters---
   [host username password cfg]
   (locking ami-connection
     (swap! config merge cfg)
-    (reset! scheduler (java.util.concurrent.Executors/newSingleThreadScheduledExecutor))
+    (reset! scheduler (Executors/newSingleThreadScheduledExecutor))
+    (reset! thread-pool (Executors/newCachedThreadPoolExecutor))
     (let [c (reset! ami-connection
                     (-> (ManagerConnectionFactory. host username password)
                         .createManagerConnection))]
@@ -502,6 +534,7 @@ cfg: a map of configuration parameters---
   []
   (locking ami-connection
     (when-let [c @ami-connection] (reset! ami-connection nil) (.logoff c))
-    (reset! scheduler nil)
-    (doseq [a [amiq-cnt-agnts agnt-state rndkey-agnt agnt-unsubscriber agnt-gc memory]]
+    (doseq [executor-atom [scheduler thread-pool]]
+      (when-let [ex @executor-atom] (reset! executor-atom nil) (.shutdown ex)))
+    (doseq [a [amiq-cnt-agnts agnt-state ticket-agnt agnt-unsubscriber agnt-gc memory]]
       (reset! a {}))))
