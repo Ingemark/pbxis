916d8d3b439fe4164706e7f85b743fbceb9e986c
Marko Topolnik <mtopolnik@inge-mark.hr>
2012-11-01 09:54:52 +0100

    Move to 0.3.2 SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis "0.3.2"
+(defproject com.ingemark/pbxis "0.3.2-SNAPSHOT"
   :description "Asterisk Call Center Adapter"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
