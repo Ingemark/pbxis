81d7ce56d75d447d7e81b35ed55b8544c0a75a36
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-04-02 13:32:44 +0200

    Upgrade lamina

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
-                 [lamina "0.5.0-beta12"]
+                 [lamina "0.5.0-beta15"]
                  [org.slf4j/slf4j-api "1.7.2"]
                  [org.slf4j/log4j-over-slf4j "1.7.2"]
                  [ch.qos.logback/logback-classic "1.0.9"]
