85784fa20cd77a397714f713381d739ad4026d7e
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-04-05 15:23:04 +0200

    upgrade lein-release

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Asterisk Call Center "
   :url "https://github.com/Inge-mark/pbxis"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :lein-release {:deploy-via :clojars}
+  :lein-release {:deploy-via :lein-deploy-clojars}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
@@ -12,6 +12,6 @@
                  [ch.qos.logback/logback-classic "1.0.9"]
                  [org.asteriskjava/asterisk-java "1.0.0.M3"]]
   :exclusions [log4j/log4j]
-  :plugins [[com.ingemark/lein-release "2.1.1"]]
+  :plugins [[com.ingemark/lein-release "2.1.2-SNAPSHOT"]]
   :jvm-opts ["-Dlogback.configurationFile=logback.xml"]
   :repl-options {:init-ns com.ingemark.pbxis})
