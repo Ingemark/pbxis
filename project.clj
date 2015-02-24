fd9d20c99641528d4742b7c4cd0d5500b70f1939
Marko Topolnik <marko.topolnik@inge-mark.com>
2015-02-24 12:43:50 +0100

    New release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis "0.6.2-SNAPSHOT"
+(defproject com.ingemark/pbxis "0.6.2"
   :description "Asterisk Call Center "
   :url "https://github.com/Inge-mark/pbxis"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
