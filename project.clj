89c584c9d46d1b540b230eacf544be6f6ce7bc4a
Viktor Matic <viktor@inge-mark.com>
2016-07-13 10:44:10 +0200

    New version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis "1.0.3-SNAPSHOT"
+(defproject com.ingemark/pbxis "1.0.3"
   :description "Asterisk Call Center "
   :url "https://github.com/Inge-mark/pbxis"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
