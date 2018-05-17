36f7dafaded438fe1e319f09d2d6881ec36a74f4
Josip Gracin <josip.gracin@ingemark.com>
2018-05-17 10:43:05 +0200

    Upgraded to newer version of lamina (0.5.6) and slf4j/logback

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis "2.0.2"
+(defproject com.ingemark/pbxis "2.0.3-SNAPSHOT"
   :description "Asterisk Call Center "
   :url "https://github.com/Inge-mark/pbxis"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -6,10 +6,10 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.incubator "0.1.2"]
-                 [lamina "0.5.0-beta15"]
-                 [org.slf4j/slf4j-api "1.7.2"]
-                 [org.slf4j/log4j-over-slf4j "1.7.2"]
-                 [ch.qos.logback/logback-classic "1.0.9"]
+                 [lamina "0.5.6"]
+                 [org.slf4j/slf4j-api "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [org.asteriskjava/asterisk-java "2.0.0.alpha-b1"]]
   :exclusions [log4j/log4j]
   :aliases {"release" ["xdo" "git-check-clean"
