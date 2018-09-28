b40e1b22fbda875320f79dc39d4d8dce03dceda8
Josip Gracin <josip.gracin@gmail.com>
2018-09-28 08:48:57 +0200

    set release version 2.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis "2.0.4-SNAPSHOT"
+(defproject com.ingemark/pbxis "2.0.4"
   :description "Asterisk Call Center Adapter"
   :url "https://github.com/Inge-mark/pbxis"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
