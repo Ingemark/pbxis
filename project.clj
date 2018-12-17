e5e66ad72a7f0824a74c00149f39105a56ca0fab
Josip Gracin <josip.gracin@gmail.com>
2018-12-17 10:43:25 +0100

    New snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis "2.0.7"
+(defproject com.ingemark/pbxis "2.0.8-SNAPSHOT"
   :description "Asterisk Call Center Adapter"
   :url "https://github.com/Inge-mark/pbxis"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
